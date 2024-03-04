2e775c0d29cbf73dbc1a64f91160879b94b0a0a5
Ryan <64923468+IrishNova@users.noreply.github.com>
2024-03-04 11:48:20 -0600

    Update under_construction.py

--- under_construction.py
+++ under_construction.py
@@ -9,51 +9,6 @@ import pandas as pd
 import random
 
 
-class TradeBook:  # TODO ultimately this will be tied in to MongoDB
-    def __init__(self):
-        self.aum = 100000
-        self.fee = 0.50  # Not many people get this treatment, but I happen to work for a guy who does. Adjust it according to YOUR trading costs.
-        self.long_stop = 0.025
-        self.long_limit = 0.015
-        self.short_limit = 0.025
-        self.long_stop = 0.015
-        self.trade_log = {}
-
-    def slippage(self, price):
-        slippage_percentage = random.uniform(0,
-                                             0.0025)  # 0.25% max, need to measure slippage of CL in future research project
-        slippage_amount = price * slippage_percentage
-        return slippage_amount
-
-    def stop_target(self, price,
-                    direction):  # day one, week one risk management here people. Probalby will move this to another Class long at some point.
-        high_mark = price + (price * self.long_limit)
-        low_mark = price - (price * self.long_stop)
-        if direction:
-            return high_mark, low_mark
-        else:
-            return low_mark, high_mark
-
-    def position_size(self, price):
-        return self.aum // price  # Place holder Not the way to do it, needs to model with leverage. Ultimately this will be another Class w/it's own risk management.
-
-    def slip_generator(self, time, price, direction):
-        if direction:
-            paid = price + self.slippage(price)
-            self.trade_log[time] = {"transaction_price": paid,
-                                    "direction": direction,
-                                    "contracts": self.position_size(price)}
-        else:
-            paid = price - self.slippage(price)
-            self.trade_log[time] = {"transaction_price": paid,
-                                    "direction": direction}
-
-    def place_order(self, time, price, direction):
-        high_mark, low_mark = self.stop_target(price, direction)
-        self.slip_generator(time, price, direction)
-        return high_mark, low_mark
-
-
 class SimulatedTrading:
 
     def __init__(self, ema1, ema2, trade_book):
@@ -76,11 +31,12 @@ class SimulatedTrading:
         self.simulation()
 
     def data_fetch(self):
-        x = pd.read_csv('CL_minute_full.csv')
+        x = pd.read_csv('/Users/ryanmoloney/Desktop/Furvus_Research/PortfolioProject/CL_minute_full.csv')
         self.raw = x.set_index(x.time).tail(2500)
 
-    def signal_generation(
-            self):  # real-world, these signals are all generated in their own Class becuase they're propritary. You're thick if you try to make money with the ones here.
+    def signal_generation(self):
+        """Real-world, these signals are all generated in their own Class because they're proprietary. 
+        You're thick if you try to make money with the ones here."""
         self.raw['delta'] = self.raw.closePrice.pct_change()
         self.raw['signal_1'] = self.raw.closePrice.ewm(span=self.span_1, adjust=False).mean()
         self.raw['signal_2'] = self.raw.closePrice.ewm(span=self.span_2, adjust=False).mean()
@@ -88,6 +44,12 @@ class SimulatedTrading:
         self.raw['signal_4'] = self.raw.signal_3.ewm(span=self.span_1 // 2, adjust=False).mean()
         self.raw['signal_5'] = self.raw.signal_3 - self.raw.signal_4
         self.raw = self.raw.dropna()
+
+    def open_position(self, direction):
+        print(self.bar)
+        # calculate position size
+        # set risk management (stops & limits)
+        # generate ticket 
 
     def long_in_play(self):
         if self.price_now > self.limit:
@@ -169,9 +131,3 @@ class SimulatedTrading:
             self.bar = i
             self.price_now = self.raw.closePrice.iloc[i]
             self.first_level_logic()
-
-
-trade_bk = TradeBook()
-ts = SimulatedTrading(30, 60, trade_bk)
-ts.run()
-print(trade_bk.trade_log)
