9b82c6963fb7838b01e869ed05251ae1e5a1191e
Ryan <64923468+IrishNova@users.noreply.github.com>
2024-03-06 16:09:14 -0600

    Create risk_management.py

---
+++ risk_management.py
@@ -0,0 +1,13 @@
+"""
+Dynamically generates stop and limits based on market conditions.
+
+Actual code, coming soon.
+"""
+
+
+import pandas as pd
+import statsmodels as sm
+
+def risk_manager(dte):
+  print('hello world')
+  
