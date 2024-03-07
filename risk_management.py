c7a0bccbdf06e91bae76efaaf246278b3973236c
Ryan <64923468+IrishNova@users.noreply.github.com>
2024-03-06 19:56:40 -0600

    Update risk_management.py
    Adds some actual code

--- risk_management.py
+++ risk_management.py
@@ -1,13 +1,91 @@
 """
 Dynamically generates stop and limits based on market conditions.
 
-Actual code, coming soon.
+Still a helluva lot of work to do. 
+
+How it works: (in theory)
+  1: Slices the the 20 days from current date.
+  2: Searches for similar periods.
+  3: (TBD) Takes the last date of each similar period
+  4: (TBD) Slices the dataframe from that date 10 periods forward
+  5: (TBD) Looks at the stdev/vol of those periods
+  6: (TBD) Uses this to set stops... more vol, looser. Less vol, tighter.
+  7: (TBD) Calculates how many ticks to allow & returns a stop-loss or limit price.
+
+Disclaimer:
+    Trading futures comes with substantial risk.
+    This app is intended for informational purposes only.
+    By using any part of this code, the user accepts
+    full ownership of the risks associated.
 """
 
 
 import pandas as pd
-import statsmodels as sm
+import numpy as np
+from sklearn.cluster import KMeans
 
-def risk_manager(dte):
-  print('hello world')
-  
+from datetime import datetime   #TODO delete this with the rest of the dummy code
+
+
+class RiskManager:
+
+    def __init__(self):
+        self.df = None
+        self.dx = None
+        self.closest_date = None
+        self.working = None
+        self.sim_x = None
+        self.load_data()
+
+    def load_data(self):
+        self.df = pd.read_csv('vix_daily_data.csv', index_col='Date', parse_dates=True)
+        self.df['delta'] = self.df.Close.pct_change()
+        self.df = self.df.dropna()
+
+    def date_match(self, d):
+        if d in self.df.index:
+            self.closest_date = d
+        else:
+            self.closest_date = self.df.index[self.df.index < d][-1]
+
+    def slice_data(self):
+        self.dx = self.df.loc[:self.closest_date].copy()
+        self.working = self.dx.tail(20)
+
+    def manage(self, d):
+        self.date_match(d)
+        self.slice_data()
+        self.finder()
+
+    def finder(self, n_clusters=5):
+
+        self.sim_x = []
+
+        rolling_windows = self.dx['delta'].rolling(window=20)
+        feature_vectors = np.array([window for window in rolling_windows if len(window) == 20])
+        if len(feature_vectors) > 0:
+            kmeans = KMeans(n_clusters=n_clusters, random_state=42)
+            kmeans.fit(feature_vectors)
+
+            working_features = np.array(self.working['delta']).reshape(1, -1)
+            working_cluster = kmeans.predict(working_features)
+
+            similar_periods_indices = np.where(kmeans.labels_ == working_cluster[0])[0]
+
+            print(similar_periods_indices)
+
+
+#TODO dummy code to be deleted 
+temp_dates = [datetime(2018, 11, 22, 11, 26),  
+              datetime(2019, 9, 15, 14, 26),
+              datetime(2020, 8, 6, 14, 26),
+              datetime(2021, 7, 16, 14, 26),
+              datetime(2022, 12, 28, 14, 26),
+              datetime(2023, 12, 28, 14, 26)
+              ]
+
+rm = RiskManager()
+for dx in temp_dates:
+    rm.manage(dx)
+    print("-----"*25)
+
